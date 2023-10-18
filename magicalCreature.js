c13b497a521dab7b44160d76d768078c4193417d
PatrickCofie <124737771+PatrickCofie@users.noreply.github.com>
2023-10-18 22:20:20 +0000

    SPC Variables and Data Types

--- magicalCreature.js
+++ magicalCreature.js
@@ -1,21 +1,23 @@
 // Welcome to the magical zoo! Your task is to create and manage a list of magical creatures. Let's have some fun!
 
 // 1. Declare a variable named 'creatureName' and assign it a magical creature's name as a string.
-
+let creatureName = "dragon";
 
 
 // 2. Declare a variable named 'creatureDescription' and describe its magical properties as a string.
-
+let creatureDescription = "FireMagic";
 
 // 3. Do you believe that this creature is friendly to humans? 
 // Declare a variable named 'isFriendly' and assign it a boolean value.
-
+let isFriendly = true;
 
 
 // 4. The magical zoo has an age restriction! Declare a variable named 'visitorAge' and assign it a random age between 1 and 60.
 // Hint: Use Math.floor() to round a number up.
 // Hint: Use Math.random() for generating random numbers.
-
+let visitorAge = Math.random(60);
+visitorAge = Math.floor(visitorAge);
+console.log(visitorAge);
 
 
 // 5. Determine if the visitor is allowed to see the creature.
